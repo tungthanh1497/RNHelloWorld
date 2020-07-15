ab834116f2f68e0ce05a76cf052489ca502ec4d7
tungtt <tungthanh1497@gmail.com>
2020-07-15 16:01:20 +0700

    test commit

---
+++ components/HelloWorld.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+export default class HelloWorld extends Component {
+  render() {
+    let greeting = '\n\n\n\nHello bitch';
+    return <Text>{greeting}</Text>;
+  }
+}
