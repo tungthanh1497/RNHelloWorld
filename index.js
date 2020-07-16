bf4d2531abaeb0e3c3b9d4e416f6a9da63a319bc
tungtt <tungthanh1497@gmail.com>
2020-07-16 09:57:02 +0700

    state

--- index.js
+++ index.js
@@ -3,10 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
-import HelloWorld from './components/HelloWorld';
-import Robot from './components/Robot';
-import MultipleGreeting from './components/MultipleGreeting';
+import TextBlink from './components/TextBlink';
 
-AppRegistry.registerComponent(appName, () => MultipleGreeting);
+AppRegistry.registerComponent(appName, () => TextBlink);
