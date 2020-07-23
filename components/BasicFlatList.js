53c9b733bb45d317c4fc197826f914fd708700ac
tungtt <tungthanh1497@gmail.com>
2020-07-23 15:22:48 +0700

    flatList with image

--- components/BasicFlatList.js
+++ components/BasicFlatList.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
-import {AppRegistry, Text, View, StyleSheet, FlatList} from 'react-native';
+import {
+  AppRegistry,
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  Image,
+} from 'react-native';
 import flatListData from '../data/FlatListData';
 
 export default class BasicFlatList extends Component {
@@ -23,13 +30,25 @@ export default class BasicFlatList extends Component {
 class ItemFlatList extends Component {
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: this.props.index % 2 == 0 ? 'cyan' : 'yellow',
-        }}>
-        <Text style={styles.flatListItem}>{this.props.item.name}</Text>
-        <Text style={styles.flatListItem}>{this.props.item.description}</Text>
+      <View style={{flex: 1, flexDirection: 'column'}}>
+        <View
+          style={{
+            flexDirection: 'row',
+            flex: 1,
+            //   backgroundColor: this.props.index % 2 == 0 ? 'cyan' : 'yellow',
+            backgroundColor: 'mediumseagreen',
+          }}>
+          <Image
+            source={{uri: this.props.item.imageUrl}}
+            style={{width: 50, height: 50}}></Image>
+          <View style={{flex: 1, flexDirection: 'column'}}>
+            <Text style={styles.flatListItem}>{this.props.item.name}</Text>
+            <Text style={styles.flatListItem}>
+              {this.props.item.description}
+            </Text>
+          </View>
+        </View>
+        <View style={{height: 1, backgroundColor: 'white'}}></View>
       </View>
     );
   }
@@ -37,7 +56,7 @@ class ItemFlatList extends Component {
 
 const styles = StyleSheet.create({
   flatListItem: {
-    color: 'tomato',
+    color: 'white',
     padding: 10,
     fontSize: 16,
   },
--- data/FlatListData.js
+++ data/FlatListData.js
@@ -3,31 +3,43 @@ var flatListData = [
     key: 'qdTM2fRuSj',
     name: 'Orange and venison soup',
     description: 'Blood orange and venison combined into chunky soup',
+    imageUrl:
+      'https://smaller-pictures.appspot.com/images/dreamstime_xxl_65780868_small.jpg',
   },
   {
     key: 'WSMscnY27K',
     name: 'Fresh lobster and garlic powder served on a bed of lettuce',
     description: 'This is a cup of tea',
+    imageUrl:
+      'https://image.freepik.com/free-vector/opposite-adjective-big-small_1308-2842.jpg',
   },
   {
     key: 'OUgUEDovDS',
     name: 'Basil and rhubarb bread',
     description: 'Crunchy bread made with dried basil and fresh rhubarb',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/7/71/Small_pie.svg',
   },
   {
     key: 'RjPoRDbXTR',
     name: 'Clementine and squash curry',
     description: 'Hot curry made with fresh clementine and butternut squash',
+    imageUrl:
+      'https://ejoy-english.com/blog/wp-content/uploads/2018/02/bigandsmall-e1412899046917.jpg',
   },
   {
     key: 'vtWGBjDFQk',
     name: 'Caraway and courgette stir fry',
     description: 'Crunchy stir fry featuring fresh caraway and courgette',
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRZ3DXGLxM5zJ-SgrBxMpoUUqoi2WbBb9aKDg&usqp=CAU',
   },
   {
     key: 'sKLuC8QlQs',
     name: 'Prune and banana muffins',
     description: 'Fluffy muffins made with fresh prune and banana',
+    imageUrl:
+      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/chihuahua-dog-running-across-grass-royalty-free-image-1580743445.jpg',
   },
 ];
 
